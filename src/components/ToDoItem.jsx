86781b3ac1e675bedd638f56747818c05310b0a8
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-02-13 17:14:33 -0500

    used ternary to check if true or false for line decoration

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 
 function ToDoItem(props) {
-  function handleClick() {}
+  const [isDone, setIsDone] = useState(false);
+
+  function handleClick() {
+    setIsDone((prev) => {
+      return !prev;
+    });
+  }
 
   return (
     <div onClick={handleClick}>
-      <li style={{ textDecoration: "line-through" }}>{props.text}</li>;
+      <li style={{ textDecoration: isDone ? "line-through" : "none" }}>
+        {props.text}
+      </li>
+      ;
     </div>
   );
 }
